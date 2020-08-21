0ae217a4d01a5548543f9660e396d293fa4d3a13
Andres Quevedo <me@andresquevedo.com>
2020-08-21 17:09:22 -0500

    docs agregado

---
+++ docs/precache-manifest.7502768ed7c742f494cc67bbbafad133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1ac937387f26879d237c10f4159be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "960d7bdff3cadbd94822",
+    "url": "/static/css/main.702fce3d.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960d7bdff3cadbd94822",
+    "url": "/static/js/main.4582b333.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
